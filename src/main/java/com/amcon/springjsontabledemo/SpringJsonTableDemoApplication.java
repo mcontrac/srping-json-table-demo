8440398889f9fc2c06190c23c05aa326ac2bb249
mcontrac <milindcon@hotmail.com>
2023-06-02 20:46:47 +0800

    New Project for Json Object handling

---
+++ src/main/java/com/amcon/springjsontabledemo/SpringJsonTableDemoApplication.java
@@ -0,0 +1,13 @@
+package com.amcon.springjsontabledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonTableDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsonTableDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amcon/springjsontabledemo/SpringJsonTableDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amcon.springjsontabledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsonTableDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
